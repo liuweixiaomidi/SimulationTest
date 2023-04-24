ae7442ac8dfbc73904c32ccd4cc880654ecc3afa
ding yubo <Dingyb@seer-group.com>
2023-04-24 15:47:37 +0800

    自动发单对话交互

--- Lib/interaction.py
+++ Lib/interaction.py
@@ -1,4 +1,9 @@
+import random
+import time
+
 import Lib.function as core
+
+
 def space():
     print()
 
@@ -16,12 +21,16 @@ def check_err(value: any, scope: list):
     return False
 
 
+global terminate_condition
+
+
 def conversation():
     """
     运行后的对话信息
     用于进行策略选择
     :return: None
     """
+    global terminate_condition
     space()
     print('仿真测试发单设置: ')
     space()
@@ -65,24 +74,36 @@ def conversation():
             space()
             load_bin = core.set_target_list(load_start, load_end, load_single, load_title, load_fill)
             unload_bin = core.set_target_list(unload_start, unload_end, unload_single, unload_title, unload_fill)
-            terminate_type = int(input('终止条件选择:\n   1: 取货点用光后终止发单\n   2: 放货点用光后终止发单\n   3: 发送订单一定数量后终止发单\n   请选择: '))
-            space()
             robot_group = input('输入执行订单的机器组名称, 如果无须指定机器人组, 请输入0: ')
             if robot_group == '0':
                 robot_group = None
-            robot_label = None if input('输入执行订单的机器人标签名称, 如果无须指定标签, 请输入0: ') == '0' else input()
-            print(robot_label)
+            robot_label = input('输入执行订单的机器人标签名称, 如果无须指定标签, 请输入0: ')
+            if robot_label == '0':
+                robot_label = None
+            sleep_time = int(input('输入相邻两订单间的发单间隔s: '))
+            space()
+            terminate_type = int(input('终止条件选择:\n   1: 取货点用光后终止发单\n   2: 放货点用光后终止发单\n   3: 发送订单一定数量后终止发单\n   请选择: '))
+            index = 0
             match terminate_type:
                 case 1:
-                    pass
+                    terminate_condition = len(load_bin)
                 case 2:
-                    pass
+                    terminate_condition = len(unload_bin)
                 case 3:
-                    pass
+                    num = input('   发送多少订单后停止发单: ')
+                    terminate_condition = index != num
                 case _:
                     print('ERROR: 输入的数据不在提供的选择范围内！！！')
+            while terminate_condition:
+                index += 1
+                cur_load = random.choice(load_bin)
+                cur_unload = random.choice(unload_bin)
+                core.load_unload_order([cur_load, cur_unload], group=robot_group, label=robot_label)
+                load_bin.remove(cur_load)
+                unload_bin.remove(cur_unload)
+                time.sleep(sleep_time)
         case 2:
-            pass
+            print('暂不支持')
         case 3:
             print('暂不支持')
         case _:
