3ed64508c8137b67481371c25f0f794bbbdb8d83
ding yubo <Dingyb@seer-group.com>
2023-06-28 16:04:33 +0800

    继续简化

--- Project/DoubleCircle.py
+++ Project/DoubleCircle.py
@@ -14,29 +14,6 @@ def assign_value(probability, value):
     return value
 
 
-def old_strategy():
-    inside_points = core.set_target_list(42, 68, title='LM', remove=['LM51', 'LM55', 'LM61'])
-    outside_points = core.set_target_list(1, 40, title='LM', remove=['LM11', 'LM18', 'LM31', 'LM38'])
-    _probability = [0.8, 0.1, 0.1]
-    _value = ['null', 'in->out', 'out->in']
-    count = 0
-    while count < 100:
-        choice = assign_value(_probability, _value)
-        match choice:
-            case 'null':
-                core.goto_order(random.choice(inside_points), label='inside')
-                core.goto_order(random.choice(outside_points), label='outside')
-            case 'in->out':
-                core.goto_order(random.choice(outside_points), label='inside')
-            case 'out->in':
-                core.goto_order(random.choice(inside_points), label='outside')
-            case _:
-                print('sth goes wrong!!!')
-        if not count % 20:
-            time.sleep(30)
-        count += 1
-
-
 def get_finished_robots(robots) -> list:
     result = []
     for robot in robots:
@@ -67,9 +44,6 @@ def new_strategy():
             }
             available_points = options.get(choice)
             target = random.choice([p for p in available_points if p != cur_sta])
-            # target = random.choice([in_points for in_points in inside_points if in_points != cur_sta]) if robot in \
-            #     inside_robots else random.choice([ou_points for ou_points in outside_points if ou_points != cur_sta])
-            # target = random.choice(inside_points) if robot in inside_robots else random.choice(outside_points)
             core.goto_order(target, robot)
             count += 1
         if count == 100:
