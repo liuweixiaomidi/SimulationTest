64188bdb4517a29930759c5b1b07473f6ec8124c
ding_yu_bo <15944065615@163.com>
2023-09-13 21:23:59 +0800

    add: check column names

--- Project/SimOrderWithLabel.py
+++ Project/SimOrderWithLabel.py
@@ -28,6 +28,11 @@ def get_column_element(column_data):
         return None
 
 
+def check_column_name(columns: set):
+    if len(columns) != 1:
+        raise ValueError(columns, "列表名称不统一")
+
+
 def run():
     if num_columns % 4:
         raise ValueError(num_columns, "数据缺失, 表格的列数量不满足要求")
@@ -35,6 +40,10 @@ def run():
     unloads = list()
     rhythms = list()
     labels_ = list()
+    pickups_check = set()
+    unloads_check = set()
+    rhythms_check = set()
+    labels_check_ = set()
     # 解析 csv 数据
     for column_name in csv_file.columns:
         column_data = csv_file[column_name]
@@ -44,23 +53,31 @@ def run():
         column_index = csv_file.columns.get_loc(column_name) + 1
         match column_index % 4:
             case 1:
+                pickups_check.add(column_name[0:7])
                 check_duplicated(column_data, column_name)
                 pickups.append(temp_list)
             case 2:
+                unloads_check.add(column_name[0:7])
                 check_duplicated(column_data, column_name)
                 unloads.append(temp_list)
             case 3:
+                rhythms_check.add(column_name[0:7])
                 if column_data.nunique() != 1:
                     raise ValueError(column_name, "同一个流程的节拍不一致")
                 filter_rhythm = csv_file.dropna(subset=column_name)
                 rhythms.append(filter_rhythm[column_name].mean())
             case 0:
+                labels_check_.add(column_name[0:5])
                 if column_data.nunique() != 1:
                     raise ValueError(column_name, "同一个流程的节拍不一致")
                 filter_rhythm = csv_file.dropna(subset=column_name)
                 labels_.append(get_column_element(filter_rhythm[column_name]))
             case _:
                 raise ValueError("数学不存在了")
+    check_column_name(pickups_check)
+    check_column_name(unloads_check)
+    check_column_name(rhythms_check)
+    check_column_name(labels_check_)
     # 发单
     print("Calculate:  " + "column: " + str(num_columns // 4) + ";  pickups: " + str(len(pickups)) + ";  unloads: " +
           str(len(unloads)) + ";  rhythms: " + str(len(rhythms)) + ";  labels: " + str(len(labels_)))
